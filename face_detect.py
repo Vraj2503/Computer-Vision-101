882e3b044ec86b04197ef36d521bd4efadd6eb59
Vraj <vrajpatel250304@gmail.com>
2024-01-07 20:45:45 +0530

    This is the final commit of the repo

--- face_detect.py
+++ face_detect.py
@@ -1,6 +1,6 @@
 import cv2 as cv 
 
-img = cv.imread("Photos/group 2.jpg")
+img = cv.imread("Photos/us.jpg")
 cv.imshow("Image", img)
 
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
@@ -8,14 +8,14 @@ cv.imshow("Gray", gray)
 
 haar_cascade = cv.CascadeClassifier('haar_face.xml')
 
-faces_rect = haar_cascade.detectMultiScale(gray, scaleFactor=1.9, minNeighbors=1)
+faces_rect = haar_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=6)
 
 print(f'Number of faces = {len(faces_rect)}')
 
 for (x,y,w,h) in faces_rect:
     cv.rectangle(img, (x,y), (x+w, y+h), (0,255,0), thickness=2)
 
-cv.imshow("Objects Detected", img)
+cv.imshow("Faces Detected", img)
 
 
     
